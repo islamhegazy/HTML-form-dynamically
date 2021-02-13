762fd2d7015aefd9e0c0f8e333656c56588c4ea7
islam hegazy <islam.ab.hegazy@gmail.com>
2021-02-13 12:19:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { FormGroup } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'HTML-forms-dynamically';
+export class AppComponent implements OnInit{
+
+  form = new FormGroup({});
+  model = {};
+  setFields :boolean = false;
+
+  fields = [
+    {
+      key: 'input',
+      type: 'input',
+      templateOptions: {
+        label: 'Input',
+        placeholder: 'Input placeholder',
+        required: true,
+        change: (field, event) => {console.log(event)}
+      }
+    },
+    {
+      key: 'textarea',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Textarea',
+        placeholder: 'Textarea placeholder',
+        required: true,
+        rows:3
+      }
+    },
+    {
+      key: 'checkbox',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Checkbox',
+      }
+    },
+    {
+      key: 'select',
+      type: 'select',
+      templateOptions: {
+        label: 'Select',
+        placeholder: 'Select placeholder',
+        required: true,
+        options: [
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]
+      }
+    },
+    {
+      key: 'radio',
+      type: 'radio',
+      templateOptions: {
+        label: 'Radio',
+        required: true,
+        options: [
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+        ]
+      }
+    }
+  ];
+
+  JSONFields: FormlyFieldConfig [] = JSON.parse(JSON.stringify(this.fields));
+
+  constructor(private cdRef: ChangeDetectorRef){}
+
+  ngOnInit(): void {
+    this.setFields = true;
+    this.cdRef.detectChanges(); 
+  }
+  onChangeJson(e){
+   try {
+    this.fields =JSON.parse(e.target.value);
+    this.JSONFields= JSON.parse(JSON.stringify(this.fields));
+   } catch (error) {
+     
+   }
+  }
+
+  onSubmit(){
+    if (this.form.valid) {
+      console.log(this.form)
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
